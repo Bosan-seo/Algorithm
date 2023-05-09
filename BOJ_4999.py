a04f9b5afa3bcdce16e7e565f289798e0561bc1e
bosan <bosani02@naver.com>
2023-05-09 16:26:49 +0900

    2023/05/09

--- BOJ_4999.py
+++ BOJ_4999.py
@@ -1,10 +1,8 @@
 # 백준_4999번
+P = input()
+D = input()
 
-while True:
-    P = input()
-    D = input()
-
-    if len(P) >= len(D):
-        print("go")
-    else:
-        print("no")
+if len(P) >= len(D):
+    print("go")
+else:
+    print("no")
