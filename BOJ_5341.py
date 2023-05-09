d32bf8a908e041ca5648ffb6be1b6ac533c7e2f9
bosan <bosani02@naver.com>
2023-05-09 18:48:42 +0900

    2023/05/09

---
+++ BOJ_5341.py
@@ -0,0 +1,14 @@
+# 백준_5341번
+
+
+def Pyramids(number):
+    return number * (number + 1) // 2
+
+if __name__ == "__main__":
+    while True:
+        number = int(input())
+        
+        if number == 0:
+            break
+            
+        print(Pyramids(number=number))
