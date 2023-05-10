53ff2a72ea9cb6e28c0bae7a7d78b7db7d6b8008
bosan <bosani02@naver.com>
2023-05-10 14:38:14 +0900

    2023/05/10

--- BOJ_5597.py
+++ BOJ_5597.py
@@ -14,15 +14,18 @@ print(unsubmit[0])
 print(unsubmit[1])
 
 # chatgpt가 제시한 최적화된 코드
+# 1. 학생 명단 생성
 # num_list = list(range(1,31))
-# submit_list = []
 
-# for _ in range(28):
-#     submit_list.append(int(input()))
+# 2. 과제 제출 명단 생성
+# submit_list = [int(input()) for _ in range(28)]
 
+# 3. 미제출자 찾기
 # unsubmit = [item for item in num_list if item not in submit_list]
 
+# 4. 미제출자 출석번호 출력
 # print(unsubmit[0])
 # print(unsubmit[1])
+
 
 # 차이점: 내가 작성한 코드는 불필요한 연산이 많다. for 루프를 사용했다면 더 간결하게 작성할 수 있음.
